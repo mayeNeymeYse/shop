2bc33de7e34ce83290d2845978118e71d8428bf3
Noah Daën <daennoah@gmail.com>
2025-06-04 19:58:35 +0200

    Setup GitHub Pages avec env.js

---
+++ env.js
@@ -0,0 +1,10 @@
+window.ENV = {
+    ADMIN_USERNAME: '{{GITHUB_ADMIN_USERNAME}}',
+    ADMIN_PASSWORD: '{{GITHUB_ADMIN_PASSWORD}}',
+
+    SESSION_DURATION_MINUTES: 60,
+    APP_NAME: 'Ma Galerie',
+    DEBUG_MODE: false
+};
+
+console.log('🔧 Configuration env.js chargée');
